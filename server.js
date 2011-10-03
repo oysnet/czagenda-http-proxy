970858c710b43cb482349edbefe27e77041c5af0
David Charbonnier <d.charbonnier@oxys.net>
2011-10-03 09:01:10 +0200

    remove commented code

--- server.js
+++ server.js
@@ -24,30 +24,3 @@ var http = require('http');
 var server = http.createServer(function(req,res) {
   p(req,res,servers.get());
 }).listen(8000);
-
-/*
-var server = httpProxy.createServer(function(req,res,proxy) {
-  var target = servers.get();
-  function proxyError(err, req,res) {
-    console.log(err);
-    servers.failed(target);
-    res.writeHead(500, {
-      'Content-Type': 'text/plain'
-    });
-    res.end('aie');
-  }
-  p= proxy.proxyRequest(req, res,target);
-  p.removeAllListeners('proxyError');
-  p.removeAllListeners('end');
-  p.addListener('proxyError', proxyError);
-  p.once('end', function(socket,res) {
-    if(res.statusCode >= 500) {
-      servers.failed(target);
-    } else if(res.statusCode < 500) {
-      servers.succeed(target);
-    }
-    p.removeListener('proxyError', proxyError);
-  });
-});
-server.listen(8000);
-*/
